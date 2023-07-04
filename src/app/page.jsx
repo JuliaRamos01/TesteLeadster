9dc29ef4b4a4008d2ed2d71cd678e70a3b48b481
Julia Ramos <julia.jrra@gmail.com>
2023-07-03 22:01:41 -0300

    commit inicial

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import * as S from "@/app/components/GlobalStyle";
+import Header from "@/app/components/Header/Header";
+
+export default function Home(){
+  return(
+    <>
+    <S.GlobalStyle />
+    <Header />
+    <h1>Olá</h1>
+    </>
+  )
+};
